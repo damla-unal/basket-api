575d5d70e024190eda1259b32dc200ffccc4374a
damla-unal <damlaunal.cs@gmail.com>
2022-08-06 14:08:01 +0300

    add select queries to get customers' cart

--- db/query/cart.sql
+++ db/query/cart.sql
@@ -2,3 +2,20 @@
 INSERT INTO cart (total_price, vat, discount, status, customer_id)
 VALUES ($1, $2, $3, $4, $5)
 RETURNING *;
+
+
+-- name: GetSavedCartByCustomerID :one
+SELECT cart.id,
+       total_price,
+       vat,
+       discount,
+       status,
+       customer_id,
+       c.name as customer_name,
+       cart.created_at,
+       updated_at
+FROM cart
+         LEFT JOIN customer c on c.id = cart.customer_id
+WHERE c.id = $1
+  and cart.status = 'saved';
+
--- db/query/cart_item.sql
+++ db/query/cart_item.sql
@@ -4,3 +4,11 @@ INSERT INTO cart_item (quantity,
                        product_id)
 VALUES ($1, $2, $3)
 RETURNING *;
+
+
+-- name: GetCartItemsByCartID :many
+SELECT p.title as product_title, cart_id, quantity, cart_item.discount, product_id
+FROM cart_item
+         LEFT JOIN cart c on c.id = cart_item.cart_id
+         LEFT JOIN product p on p.id = cart_item.product_id
+WHERE cart_id = $1;
